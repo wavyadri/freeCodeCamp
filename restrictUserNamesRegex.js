3fe402b983750eefffe2d662e2617d3d242d349d
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-02-12 23:44:29 -0500

    Completed Challenge
    Practicing regular expressions.

---
+++ restrictUserNamesRegex.js
@@ -0,0 +1,18 @@
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; 
+let result = userCheck.test(username);
+
+// Restrict Possible Usernames
+// Usernames are used everywhere on the internet. They are what give users a unique identity on their favorite sites.
+
+// You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
+
+// Usernames can only use alpha-numeric characters.
+
+// The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+
+// Username letters can be lowercase and uppercase.
+
+// Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+
+// Change the regex userCheck to fit the constraints listed above.
