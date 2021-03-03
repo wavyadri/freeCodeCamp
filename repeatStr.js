99693ded6a17ca614d4d9e7451cea6469461c049
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-03-03 14:01:42 -0500

    Completed Challenge

---
+++ repeatStr.js
@@ -0,0 +1,11 @@
+function repeatStringNumTimes(str, num) {
+    if (num < 1) {
+      return "";
+    } else {
+      return str + repeatStringNumTimes(str, num - 1);
+    }
+  }
+  
+  repeatStringNumTimes("abc", 3);
+
+  // recursion
