e7aea57e65130ed1703ce748eaaac03f08fefcfe
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-03-08 17:11:04 -0500

    Completed Challenge

---
+++ hasOwnProperty.js
@@ -0,0 +1,16 @@
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+  }
+  
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  // Only change code below this line
+  for (let property in canary) {
+    if(canary.hasOwnProperty(property)) {
+      ownProps.push(property);
+    }
+  }
+
+  // Add the own properties of canary to the array ownProps.
+
