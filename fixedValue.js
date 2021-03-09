19b70ec7d3059dc9c93e4831d2a584f23e7fa43a
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-03-09 18:42:22 -0500

    Completed Challenge

---
+++ fixedValue.js
@@ -0,0 +1,11 @@
+// Fill in the code for the function incrementer so it returns the value of the global variable fixedValue increased by one.
+
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Only change code below this line
+  return fixedValue + 1;
+
+  // Only change code above this line
+}
