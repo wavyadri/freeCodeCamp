fc0dd02d99e8f016a4d9448bf50683531390845a
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-03-17 22:56:59 -0400

    Completed Challenge

---
+++ strToArrToStr.js
@@ -0,0 +1,9 @@
+function sentensify(str) {
+    // split str into an array
+    // join arr into a str with spaces
+    return str.split(/\W/).join(" ");
+  }
+
+  // Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. 
+  // The function should return a string. For example, I-like-Star-Wars would be converted to I like Star Wars. 
+  // For this challenge, do not use the replace method.
