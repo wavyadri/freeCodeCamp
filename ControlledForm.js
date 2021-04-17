4ed9a114eb3eb373dda8ed69c0579139d50d77cd
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-04-17 12:01:08 -0400

    Create ControlledForm.js

---
+++ ControlledForm.js
@@ -0,0 +1,45 @@
+class MyForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: '',
+        submit: ''
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+      this.setState({
+        input: event.target.value
+      });
+    }
+    handleSubmit(event) {
+      // Change code below this line
+      // prevent the default form submit behavior which will refresh the web page
+      event.preventDefault()
+  
+      // make the submit match user input
+      this.setState ({
+        submit: this.state.input
+      })
+      // Change code above this line
+    }
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            {/* Change code below this line */}
+            <input value={this.state.input} onChange={this.handleChange}></input>
+            {/* Change code above this line */}
+            <button type='submit'>Submit!</button>
+          </form>
+          {/* Change code below this line */}
+          <h1>{this.state.submit}</h1>
+          {/* Change code above this line */}
+        </div>
+      );
+    }
+  }
+
+
+  // building on the last challenge of input element, here we will build a controlled form which displays user's input upon clicking submit
