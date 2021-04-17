281055509c7c6832902e9d27c1ed34ae4b57ae07
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-04-17 16:49:39 -0400

    Update InlineStylesJSX.js

--- InlineStylesJSX.js
+++ InlineStylesJSX.js
@@ -9,3 +9,21 @@ class Colorful extends React.Component {
 // HTML style elements as a string
 // BUT in JSX they must be an object
 // no kebab-case
+
+const styles = {
+    color: 'purple',
+    fontSize: 40,
+    border: "2px solid purple",
+  };
+  
+  class Colorful extends React.Component {
+    render() {
+      // change code below this line
+      return (
+        <div style={styles}>Style Me!</div>
+      );
+      // change code above this line
+    }
+  };
+
+  // multiple styles can be stored outside in an object
